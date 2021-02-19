2dcb0d23689b3bb5caf9d67494d7a253d49c76f0
gr691_fnv <gr691_fnv@L205-1>
2021-02-19 09:57:42 +0700

    Update text

--- ExampleForGit/Program.cs
+++ ExampleForGit/Program.cs
@@ -6,7 +6,7 @@ namespace ExampleForGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
