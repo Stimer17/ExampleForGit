3998a9605d642f0cea1f64cbcb65026c4df8c312
gr691_fnv <gr691_fnv@L205-1>
2021-02-19 09:36:16 +0700

    Добавьте файлы проекта.

---
+++ ExampleForGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
